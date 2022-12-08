e5109ebaa88a7dbbe92ee7dbfabe1ed9d6e753d9
Pedro Alves <pedro.alves.hf@gmail.com>
2022-12-08 13:50:50 -0300

    third one also

--- src/getAdventurerWithHigherDamageWeapon.js
+++ src/getAdventurerWithHigherDamageWeapon.js
@@ -1,7 +1,10 @@
+const { adventurers } = require('../data/game_data');
 const data = require('../data/game_data');
 
 const getAdventurerWithHigherDamageWeapon = () => {
-  // escreva o código aqui
-};
+  const higherDamage = data.weapons.sort((a, b) => b.damage -a.damage)[1];
+    return data.adventurers.find((adventurer) => adventurer.weapons.includes(higherDamage.id))
+}
 
-module.exports = getAdventurerWithHigherDamageWeapon;
+console.log(getAdventurerWithHigherDamageWeapon())
+    module.exports = getAdventurerWithHigherDamageWeapon;
