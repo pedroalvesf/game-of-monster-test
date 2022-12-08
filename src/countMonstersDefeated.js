3ffb7c8121f6f0d3d0ea173978a838768d0af2ad
Pedro Alves <pedro.alves.hf@gmail.com>
2022-12-08 09:42:36 -0300

    second ok

--- src/countMonstersDefeated.js
+++ src/countMonstersDefeated.js
@@ -1,26 +1,36 @@
 const { monsters, adventurers } = require('../data/game_data');
 const data = require('../data/game_data');
 
-const countMonstersDefeated = () => {
-  const typeMonster = monsters.map((monster) => monster.id);
-  const objectMonsters = typeMonster.reduce((acc, curr) => {
+const getTypeMonsterById = (arrayId) => {
+  return arrayId.map((id) => {
+      const monsterFilterId =  monsters.filter((monster) => monster.id == id)
+      return monsterFilterId[0].type
+  });
+}
+
+const countMonstersDefeated = (type) => {
+  if(!type) {
+    const idMonster = monsters.map((monster) => monster.id);
+    const typeMonsterById = getTypeMonsterById(idMonster);
+    const objectMonsters = typeMonsterById.reduce((acc, curr) => {
     acc[curr] = 0
     return acc
   }, {})
-  typeMonster.map((monster) => {
-    objectMonsters[monster] = countMonster(monster);
+    idMonster.map((monster, index) => {
+    objectMonsters[typeMonsterById[index]] = countMonster(monster);
   })
   return objectMonsters;
+}
 };
 
 const countMonster = (id) => {
 const monstersDefeateds = adventurers.map(
-  (adventurer) => adventurer.defeatedMonsters).flat(1);
+(adventurer) => adventurer.defeatedMonsters).flat(1);
 const count = monstersDefeateds.filter((x) => {
   return x == id;
 })
 return count.length
 }
 
-console.log(countMonstersDefeated());
+console.log(countMonstersDefeated(''));
 module.exports = countMonstersDefeated;
