d4aaf1267269ebb8c028351d5ba8a1483ada331b
Pedro Alves <pedro.alves.hf@gmail.com>
2022-12-08 14:00:38 -0300

    fourth done

--- src/getMonstersNameBySize.js
+++ src/getMonstersNameBySize.js
@@ -2,6 +2,15 @@ const data = require('../data/game_data');
 
 const getMonstersNameBySize = () => {
   // escreva o código aqui
+    return data.monsters.reduce((acc, monster) => {
+      acc[monster.size].push(monster.name)
+    return acc
+  }, {
+    Small: [],
+    Huge: [],
+    Large: [],
+    Medium: [],
+  })
 };
-
+console.log(getMonstersNameBySize());
 module.exports = getMonstersNameBySize;
